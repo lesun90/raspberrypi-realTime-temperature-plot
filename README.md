fc3be80c1848b7f64c8d7c502ea15370211a83f5
Duong Le <lesun90@gmail.com>
2017-12-09 20:14:30 -0500

    add files

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # raspberrypi-realTime-temperature-plot
+//Look for PI ip address
+nmap -sn 192.168.1.0/24
+//install nodejs
+sudo apt-get update
+sudo apt-get dist-upgrade
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt-get install -y nodejs
+//install socket and express
+npm init
+npm install socket.io express --save
+//install serialport
+npm install serialport
+sudo npm install serialport --unsafe-perm --build-from-source
+//install Raspberry module for nodejs
+npm install onoff
---
+++ app.js
@@ -0,0 +1,73 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+var SerialPort = require('serialport');
+var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
+var pushButton = new Gpio(17, 'in', 'both'); //use GPIO pin 17 as input, and 'both' button presses, and releases should be handled
+
+
+//setup Hardware
+const parsers = SerialPort.parsers;
+const parser = new parsers.Readline({
+  delimiter: '\r\n'
+});
+var port = new SerialPort('/dev/ttyUSB0', {
+  baudRate: 9600
+});
+
+port.pipe(parser);
+port.on('open', () => console.log('Port open'));
+///////////////////////
+
+app.use(express.static(__dirname + '/node_modules'));
+app.get('/', function(req, res,next) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+server.listen(5000);
+console.log("start server...")
+
+serialdata = 0
+sampleNumber = 0
+recordData = []
+btnStatus = 0;
+io.sockets.on('connection', function (socket) {// WebSocket Connection
+  //read button status
+  pushButton.read(function (err, value) { 
+    if (err) { //if an error
+      console.error('There was an error', err); //output error message to console
+      return;
+    }
+    btnStatus = value
+    socket.emit('btnStatus', btnStatus); //send button status to client
+  });
+  //serial event
+  parser.on('data', function(data){
+    console.log(data)
+    serialdata = data
+    sampleNumber++
+    socket.emit('senddata', {'data': serialdata, 'sampleNumber': sampleNumber});
+    if (btnStatus == 1)
+    {
+      recordData.push(serialdata)
+    }
+  });
+
+  //Watch for button change status
+
+  pushButton.watch(function (err, value) { 
+    if (err) { //if an error
+      console.error('There was an error', err); //output error message to console
+      return;
+    }
+    btnStatus = value
+    socket.emit('btnStatus', btnStatus); //send button status to client
+    if (value == 0)
+    {
+      socket.emit('sendRecordData', recordData); //send button status to client
+      recordData = []
+    }
+  });
+
+});
